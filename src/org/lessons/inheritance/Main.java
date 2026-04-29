a1791c902ab00c444a9d0d98829bd6c9183809c3
MhZ9191 <hegic91@gmail.com>
2026-04-29 09:52:12 +0200

    Add class Prodotto

--- src/org/lessons/inheritance/Main.java
+++ src/org/lessons/inheritance/Main.java
@@ -1,7 +1,12 @@
 package org.lessons.inheritance;
 
+import java.math.BigDecimal;
+
 public class Main {
     public static void main(String[] args) {
         
+        Prodotto test = new Prodotto("jasmin","diesel",new BigDecimal("10"),new BigDecimal("22"));
+
+        System.out.println(test.totalPrice());
     }
 }
--- src/org/lessons/inheritance/Prodotto.java
+++ src/org/lessons/inheritance/Prodotto.java
@@ -1,5 +1,89 @@
 package org.lessons.inheritance;
 
+
+import java.math.BigDecimal;
+import java.util.Random;
+import java.math.RoundingMode;
+
 public class Prodotto {
+  
+private int code;
+private String name;
+private String brand;
+private BigDecimal price;
+private BigDecimal iva;
+
+//COSTRUTTORE
+public Prodotto(String name,String brand,BigDecimal price,BigDecimal iva){
+    Random rnd = new Random();
+    this.code=rnd.nextInt(1000,9999)*rnd.nextInt(10,20);
+    if(name!=null&&name.trim()!=""){
+        this.name=name;
+    }
+
+    if(brand!=null&&brand.trim()!=""){
+        this.brand=brand;
+    }
+
+    if(price.compareTo(BigDecimal.ZERO)==1){
+        this.price=price.setScale(2,RoundingMode.DOWN);
+    }
+
+    if(iva.compareTo(BigDecimal.ZERO)>=0){
+        this.iva=iva.setScale(2,RoundingMode.DOWN);
+    }
+}
+
+//GETTER & SETTER
+
+public int getCode(){
+    return this.code;
+}
+
+public String getName(){
+    return this.name;
+}
+
+public void setName(String name){
+    if(name!=null&&name.trim()!=""){
+        this.name=name;
+    }
+}
+
+public String getBrand(){
+    return this.brand;
+}
+
+public void setBrand(String brand){
+    if(brand!=null&&brand.trim()!=""){
+        this.brand=brand;
+    }
+}
+
+public BigDecimal getPrice(){
+    return this.price;
+}
+
+public void setPrice(BigDecimal price){
+    if(price.compareTo(BigDecimal.ZERO)==1){
+        this.price=price;
+    }
+}
+
+public BigDecimal getIva(){
+    return this.iva;
+}
+
+public void setIva(BigDecimal iva){
+    if(iva.compareTo(BigDecimal.ZERO)==1){
+        this.iva=iva;
+    }
+}
+
+
+//METODI AGGIUNTIVI
+public BigDecimal totalPrice(){
+    return this.price.add(this.price.multiply(iva).divide(new BigDecimal("100"))).setScale(2,RoundingMode.DOWN);
+}
 
 }
